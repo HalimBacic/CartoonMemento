15184e753ebb131608d1214565817640673affdc
HalimBacic <33162351+HalimBacic@users.noreply.github.com>
2021-03-24 12:16:47 +0100

    Commit after resource loading

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -15,8 +16,78 @@ namespace CartoonMemento
         public MainWindow()
         {
             InitializeComponent();
+            InitializeStickers();
         }
 
+        private void InitializeStickers()
+        {
+            //TODO Change this to avoid hardcore programming
+            string path = "D:\\Fakultet\\HCI\\PROJEKAT\\CartoonMemento\\CartoonMemento\\resources";
+
+            string[] dirs = Directory.GetDirectories(path);
+
+            foreach (string dir in dirs)
+            {
+                FileInfo dirInfo = new FileInfo(dir);
+                Expander exp = new Expander();
+                exp.Header = dirInfo.Name;
+                exp.Width = 330;
+                exp.Height = 170;
+                Grid grid = new Grid();
+                grid.Width = 328;
+                string[] files = Directory.GetFiles(dir);
+
+
+                int numOfFiles = files.Length;
+                int numOfRows;
+                if (numOfFiles % 3 == 0)
+                    numOfRows = numOfFiles / 3;
+                else
+                    numOfRows = numOfFiles / 3 + 1;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    ColumnDefinition col = new ColumnDefinition();
+                    grid.ColumnDefinitions.Add(col);
+                }
+
+                for (int i = 0; i < numOfRows; i++)
+                {
+                    RowDefinition row = new RowDefinition();
+                    grid.RowDefinitions.Add(row);
+                }
+
+                int rowNum = 0; int columnNum = 0;
+                foreach (string file in files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    Image image = new Image();
+                    Uri uri = new Uri(fi.FullName);
+                    image.Source = new BitmapImage(uri);
+                    image.Height = 100;
+                    image.Width = 100;
+
+                    Grid.SetRow(image,rowNum);
+                    Grid.SetColumn(image,columnNum);
+
+                    if (columnNum % 3 == 2)
+                    {
+                        columnNum = 0;
+                        rowNum++;
+                    }
+                    else
+                        columnNum++;
+
+                    grid.Children.Add(image);
+                }
+
+                ScrollViewer scroll = new ScrollViewer();
+                scroll.Content = grid;
+
+                exp.Content = scroll;
+                stickers.Children.Add(exp);
+            }
+        }
         private void buttonLoad_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
